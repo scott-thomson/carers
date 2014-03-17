3050c879d8f5423963754490b73fb5e394b94219
Adrian Hepworth <adrian.hepworth@gmail.com>
2014-03-17 17:25:45 +0000

    Starting business logic changes

--- src/main/scala/org/cddcore/carers/Carers.scala
+++ src/main/scala/org/cddcore/carers/Carers.scala
@@ -1,16 +1,12 @@
 package org.cddcore.carers
 
 import org.junit.runner.RunWith
-import org.cddcore.engine.Engine
+import org.cddcore.engine._
 import org.cddcore.engine.tests.CddJunitRunner
-import org.corecdd.website.WebServer
-import org.cddcore.engine.LoggerDisplay
-import org.cddcore.engine.LoggerDisplayProcessor
-import scala.xml.Elem
-import org.cddcore.engine.XmlSituation
 import org.joda.time.DateTime
-import scala.xml.XML
 import org.joda.time.format.DateTimeFormat
+import scala.xml._
+import org.joda.time.Years
 
 case class KeyAndParams(key: String, params: Any*)
 
@@ -40,18 +36,43 @@ object Xmls {
   def asDate(s: String): DateTime = formatter.parseDateTime(s);
 }
 
-case class CarersXmlSituation(w: World, validateClaimXml: Elem) extends XmlSituation
+case class CarersXmlSituation(w: World, validateClaimXml: Elem) extends XmlSituation {
+
+  import Xml._
+  lazy val claimBirthDate = xml(validateClaimXml) \ "ClaimantData" \ "ClaimantBirthDate" \ "PersonBirthDate" \ date("yyyy-MM-dd")
+  lazy val overSixteen = Carers.overSixteen(claimBirthDate(), w.dateProcessingDate)
+  lazy val claim35Hours = xml(validateClaimXml) \ "ClaimData" \ "Claim35Hours" \ yesNo(default = false)
+  lazy val ClaimCurrentResidentUK = xml(validateClaimXml) \ "ClaimData" \ "ClaimCurrentResidentUK" \ yesNo(default = false)
+  lazy val ClaimEducationFullTime = xml(validateClaimXml) \ "ClaimData" \ "ClaimEducationFullTime" \ yesNo(default = false)
+  lazy val ClaimAlwaysUK = xml(validateClaimXml) \ "ClaimData" \ "ClaimAlwaysUK" \ yesNo(default = false)
+
+}
 
 @RunWith(classOf[CddJunitRunner])
 object Carers {
-
   implicit def stringStringToCarers(x: Tuple2[String, String]) = CarersXmlSituation(World(x._1), Xmls.validateClaim(x._2))
+  implicit def stringToDate(x: String) = Xmls.asDate(x)
 
-  val engine = Engine[CarersXmlSituation, KeyAndParams]().
+  val overSixteen = Engine[DateTime, DateTime, Boolean]().title("Over sixteen").
+    useCase("Oversixteen").
+    scenario("1996-12-10", "2012-12-9").expected(false).
+    code((from: DateTime, to: DateTime) => {
+      val years = Years.yearsBetween(from, to).getYears;
+      years >= 16
+    }).
+    scenario("1996-12-10", "2012-12-10").expected(true).
+    scenario("1996-12-10", "2012-12-11").expected(true).
+    build
+
+  val engine = Engine[CarersXmlSituation, KeyAndParams]().title("Validate Claim Rules").
+    code((c: CarersXmlSituation) => KeyAndParams("Default Response")).
+    useCase("Claimants under the age of 16 are not entitled to claim Carer's Allowance", "Carer’s Allowance is intended for people over the age of 16 who are unable to undertake or continue regular full time employment because they are needed at home to look after a disabled person. Carer’s Allowance is not available to customers under the age of 16.").
+    scenario(("2010-7-25", "CL100104A"), "Claimant CL100104 is a child under 16").expected(KeyAndParams("510", "16")).
+    because((c: CarersXmlSituation) => !c.overSixteen).
     build
 
   def main(args: Array[String]) {
     val formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
-    println(engine(("2010-7-25", "CL100104A")))
+    //    println(engine(("2010-7-25", "CL100104A")))
   }
 }
